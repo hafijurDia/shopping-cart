48f2466340f346aebbab781e6516e41763a26b89
hafijure <hafijure@gmail.com>
2022-10-31 12:58:48 -0300

    price upadate

--- script.js
+++ script.js
@@ -2,17 +2,34 @@
 const increment = document.getElementById('increment');
 const decrement = document.getElementById('decrement');
 
-const itemCount = document.getElementById('item-number').value;
-let itemNumber = parseFloat(itemCount);
 
-
+//incremnet event
 increment.addEventListener('click',function(){
-    incrementDecrement(itemNumber);
+    let getInutNumber = getInputBumber('item-number');
+    incrementDecrement(getInutNumber);
+    console.log(getInutNumber);
+
+    let singleItemPrice = document.getElementById('item-price').innerText;
+    singleItemPrice = parseFloat(singleItemPrice);
+    document.getElementById('item-price').innerText = singleItemPrice * getInutNumber;
 })
 
+//decrement event
 decrement.addEventListener('click',function(){
-    incrementDecrement(itemNumber - 2 );
+    let getInutNumber = getInputBumber('item-number');
+    console.log(getInutNumber);
+    
+    if ( getInutNumber < 2) {
+        document.getElementById('item-number').value = 1;
+    }
+    else{
+        incrementDecrement(getInutNumber - 2 );
+    }
+    
+    
 })
+
+
 
 
 //common function for increment and decreament
@@ -22,7 +39,9 @@ function incrementDecrement(id){
 }
 
 //common funtion for item input value
-function getInputBumber(){
-
+function getInputBumber(id){
+    const itemCount = document.getElementById(id).value;
+    let itemNumber = parseFloat(itemCount);
+    return itemNumber;
 }
 
