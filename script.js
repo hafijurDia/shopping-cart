93bc45b9e191af0804a0df2ce058bac0c5022267
hafijure <hafijure@gmail.com>
2022-11-01 14:29:54 -0300

    shopping cart full done

--- script.js
+++ script.js
@@ -1,47 +1,71 @@
-//single item product increment and decrement
-const increment = document.getElementById('increment');
-const decrement = document.getElementById('decrement');
-
-
-//incremnet event
-increment.addEventListener('click',function(){
-    let getInutNumber = getInputBumber('item-number');
-    incrementDecrement(getInutNumber);
-    console.log(getInutNumber);
-
-    let singleItemPrice = document.getElementById('item-price').innerText;
-    singleItemPrice = parseFloat(singleItemPrice);
-    document.getElementById('item-price').innerText = singleItemPrice * getInutNumber;
-})
-
-//decrement event
-decrement.addEventListener('click',function(){
-    let getInutNumber = getInputBumber('item-number');
-    console.log(getInutNumber);
-    
-    if ( getInutNumber < 2) {
-        document.getElementById('item-number').value = 1;
+function updateProductNumber(product, price, isIncreasing){
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
+    if (isIncreasing == true) {
+        productNumber = parseInt(productNumber) + 1
     }
-    else{
-        incrementDecrement(getInutNumber - 2 );
+    else if (isIncreasing == false && productNumber > 0){
+        productNumber = parseInt(productNumber) - 1;
     }
-    
-    
-})
+    productInput.value = productNumber;
+    //update product total value
+    let productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
 
+    calculateTotal();
 
-
-
-//common function for increment and decreament
-function incrementDecrement(id){
-    itemNumber = id + 1;
-    document.getElementById('item-number').value = itemNumber;
 }
 
-//common funtion for item input value
-function getInputBumber(id){
-    const itemCount = document.getElementById(id).value;
-    let itemNumber = parseFloat(itemCount);
-    return itemNumber;
+function getInputNumber(product){
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
 }
 
+function calculateTotal(){
+    const phoneTotal  = getInputNumber('phone') * 1219;
+    const caseTotal  = getInputNumber('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const total = subTotal + tax;
+    
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax').innerText = tax;
+    document.getElementById('total').innerText = total;
+
+}
+function removeProduct(item) {
+    document.getElementById(item + '-display').style.display = 'none';
+}
+
+
+//phone increment event
+document.getElementById('phone-increment').addEventListener('click',function(){
+    updateProductNumber('phone',1219,true);
+})
+//phone decrement event
+document.getElementById('phone-decrement').addEventListener('click',function(){
+    updateProductNumber('phone',1219,false);
+})
+
+//case increment event
+document.getElementById('case-increment').addEventListener('click',function(){
+    updateProductNumber('case',59,true);
+})
+//case decrement event
+document.getElementById('case-decrement').addEventListener('click',function(){
+    updateProductNumber('case',59,false);
+})
+
+// remove item
+document.getElementById("phone-remove").addEventListener('click', function () {
+    removeProduct('phone');
+});
+document.getElementById("case-remove").addEventListener('click', function () {
+    removeProduct('case');
+});
+
+// check out
+document.getElementById("check-btn").addEventListener('click', function () {
+    alert('successful');
+});
